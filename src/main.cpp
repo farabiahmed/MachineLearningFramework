103cad8ccf31d5a38c3fce252e3c3a850b552b56
farabiahmed <farabiahmed@gmail.com>
2017-05-09 09:33:22 +0300

    User menu and switch-case structure added to test various algorithms.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 
 #include "Environments/Environment.hpp"
 #include "Environments/Gridworld.hpp"
+#include "Environments/Blocksworld.hpp"
 
 #include "QIteration/CleaningRobotDeterministic.hpp"
 #include "QIteration/GridWorld.hpp"
@@ -24,6 +25,7 @@
 
 #include "Representations/Representation.hpp"
 #include "Representations/StateActionValue.hpp"
+#include "Representations/GaussianRadialBasisFunctionApproximator.hpp"
 
 #include "Agents/Agent.hpp"
 #include "Agents/QIteration.hpp"
@@ -35,7 +37,16 @@ using namespace std;
 
 void help_menu(void)
 {
-	cout << "Reinfocement Learning							"<<endl;
+	cout << "***********************************************************************************"<<endl;
+	cout << "Deep Reinfocement Learning	Framework Main Routine									"<<endl;
+	cout << "***********************************************************************************"<<endl;
+	cout << "1- Grid World with Qiteration														"<<endl;
+	cout << "2- Grid World with Trajectory Based Q-Value Iteration and QTable Representation	"<<endl;
+	cout << "3- Grid World with Trajectory Based Q-Value Iteration and RBF Representation		"<<endl;
+	cout << "4- Blocks World with Qiteration													"<<endl;
+	cout << "***********************************************************************************"<<endl;
+	cout << "Enter your choice and press return: ";
+
 }
 
 int main()
@@ -43,39 +54,133 @@ int main()
 	// For Random Number Generator
 	srand(time(0));
 
+	// Store the user input
+	int choice=0;
+
 	// Menu for user
 	help_menu();
+
+	// Get The User Input
+	cin >> choice;
+
+	// End of user input
+	cout << "***********************************************************************************"<<endl;
 
 	// Class Pointers
 	Environment* environment;
 	Representation* value;
 	Agent* agent;
-	/*
+
 	// Select Application
-	int key=1;
-	switch (key) {
-		case 0:
-			// QIteration Example for GridWorld
-			environment = new Gridworld();
-			value = new StateActionValue();
-			agent = new QIteration(environment, value);
-			agent->Start_Execution();
-			value->Print_Value();
-			break;
-
+	switch (choice)
+	{
 		case 1:
-			// Trajectory Based Value Iteration Example for GridWorld
-			environment = new Gridworld();
-			value = new StateActionValue();
-			agent = new TrajectoryBasedValueIteration(environment, value);
-			agent->Start_Execution();
-			agent->Test();
-			break;
+		{
+			// QIteration Example for GridWorld
 
+			// Get parameters from file
+			ConfigParser cfg = ConfigParser("config/config_gridworld.cfg");
+
+			// Initialize Environment
+			environment 	= new Gridworld(cfg);
+
+			// Initialize Representation Function
+			value 			= new StateActionValue(*environment,cfg);
+
+			// Initialize Solver
+			agent 			= new QIteration(environment, value, cfg);
+
+			//Start Calculation
+			agent->Start_Execution();
+
+			//Show Q-Values
+			value->Print_Value();
+
+			//Show Policy
+			environment->Display_Policy(*value);
+		}break;
+		case 2:
+		{
+			// Trajectory Based Value Iteration with QTable Representation Example for GridWorld
+
+			// Get parameters from file
+			ConfigParser cfg = ConfigParser("config/config_gridworld.cfg");
+
+			// Initialize Environment
+			environment 	= new Gridworld(cfg);
+
+			// Initialize Representation Function
+			value 			= new StateActionValue(*environment,cfg);
+
+			// Initialize Solver
+			agent 			= new TrajectoryBasedValueIteration(environment, value, cfg);
+
+			//Start Calculation
+			agent->Start_Execution();
+
+			//Show Q-Values
+			value->Print_Value();
+
+			//Show Policy
+			environment->Display_Policy(*value);
+
+		}break;
+		case 3:
+		{
+			// Trajectory Based Value Iteration with RBF Representation Example for GridWorld
+
+			// Get parameters from file
+			ConfigParser cfg = ConfigParser("config/config_gridworld.cfg");
+
+			// Initialize Environment
+			environment 	= new Gridworld(cfg);
+
+			// Initialize Representation Function
+			value 			= new GaussianRadialBasisFunctionApproximator(*environment,cfg);
+
+			// Initialize Solver
+			agent 			= new TrajectoryBasedValueIteration(environment, value, cfg);
+
+			//Start Calculation
+			agent->Start_Execution();
+
+			//Show Q-Values
+			value->Print_Value();
+
+			//Show Policy
+			environment->Display_Policy(*value);
+
+		}break;
+		case 4:
+		{
+			// Qiteration Example for BlocksWorld
+
+			// Get parameters from file
+			ConfigParser cfg = ConfigParser("config/config_blocksworld.cfg");
+
+			// Initialize Environment
+			environment 	= new Blocksworld(cfg);
+
+			// Initialize Representation Function
+			value 			= new StateActionValue(*environment,cfg);
+
+			// Initialize Solver
+			agent 			= new QIteration(environment, value, cfg);
+
+			//Start Calculation
+			agent->Start_Execution();
+
+			//Show Q-Values
+			value->Print_Value();
+
+			//Show Policy
+			environment->Display_Policy(*value);
+
+		}break;
 		default:
+			cout<< "You have entered an invalid input."<<endl;
 			break;
 	}
-	*/
 
 	delete agent;
 	delete environment;
