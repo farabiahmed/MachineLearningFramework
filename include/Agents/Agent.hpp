01466234b0b4406f00d60ba20c9a7ee5b6177f75
farabiahmed <farabiahmed@gmail.com>
2017-05-23 13:13:09 +0300

    Common functionalities related to simulation added.

--- include/Agents/Agent.hpp
+++ include/Agents/Agent.hpp
@@ -8,8 +8,12 @@
 #ifndef AGENTS_AGENT_HPP_
 #define AGENTS_AGENT_HPP_
 
+#include <fstream> // file write for datalog
+#include <cstdlib> // system command
 #include "Environments/Environment.hpp"
 #include "Representations/Representation.hpp"
+#include "ProbabilityDistributions/ProbabilityDistribution.hpp"
+#include "ProbabilityDistributions/DiscreteDistribution.hpp"
 
 using namespace std;
 /*
@@ -18,22 +22,39 @@ using namespace std;
 class Agent {
 public:
 	Agent();
+
 	virtual ~Agent();
-	virtual bool Start_Execution() = 0;
 
-	virtual void Test(void) = 0;
+	virtual bool Start_Execution() = 0; ///< Main solver that runs the bellman iterations.
 
-	int max_number_of_iterations;
+	void Get_Report(string filePath, string fileName);
+
+	int max_number_of_iterations;	///< Stores number of max iteration to find best policy.
+
+	vector<SmartVector> states;  ///< Stores the states of the environment to increase speed
+	 	 	 	 	 	 	 	 ///< of calculation.
 
 	string Name;	///< It stores the name of agent to increase readability and awareness.
     				///< Such as Qiteration, TrajectoryBased etc.
 
-
-
 protected:
+
 	Environment* environment;
+
 	Representation* valueFunction;
 
+	void Get_Cumulative_Rewards(void);  ///< It does the simulation for predefined times
+											///< in a loop and fills the "rewards_cumulative".
+
+	double Simulate(void); 		///< Simulates the agent from an random initial point through
+								///< calculated last policy.
+
+	vector<vector<double>> rewards_cumulative;	///< Stores the rewards that collected during
+												///< each iteration. Then printed out to csv file
+												///< to be shown by python plotting libraries.
+
+	unsigned number_of_simulations;///< Holds the number of simulations that needs to
+												///< be done for performance evaluation.
 };
 
 #endif /* AGENTS_AGENT_HPP_ */
--- src/Agents/Agent.cpp
+++ src/Agents/Agent.cpp
@@ -24,3 +24,82 @@ Agent::Agent() {
 Agent::~Agent() {
 	// TODO Auto-generated destructor stub
 }
+
+void Agent::Get_Cumulative_Rewards(void)
+{
+	// Collect the performance of iteration to draw a graphic.
+	vector<double> reward_per_iteration;
+	for (unsigned i = 0; i < number_of_simulations; ++i) {
+		double r = Simulate();
+		reward_per_iteration.push_back(r);
+	}
+	rewards_cumulative.push_back(reward_per_iteration);
+}
+
+double Agent::Simulate(void)
+{
+	// Hold the reward that we will return.
+	double reward=0;
+
+	// Count the number of total movement.
+	unsigned number_of_movement = 0;
+
+	// Current State of agent
+	SmartVector state = environment->Get_Random_State();
+
+	SmartVector nextState;
+	SmartVector action;
+
+	// Run the simulation and collect rewards.
+	while( !environment->Check_Terminal_State(state) && number_of_movement<states.size() )
+	{
+		// Get next action to apply from updated policy
+		action  = valueFunction->Get_Policy(state);
+
+		// Get next state by iterating previous state and action.
+		nextState = environment->Get_Next_State(state,action);
+
+		// Sum the reward
+		reward += environment->Get_Reward(state,action,nextState);
+
+		// Move the agent to next state.
+		state = nextState;
+
+		// Increase total number of movement to be blocked in a loop in case of a bad policy.
+		number_of_movement++;
+	}
+
+	return reward;
+}
+
+void Agent::Get_Report(string filePath, string fileName)
+{
+	// Create File Directory First
+	system(("mkdir -p " + filePath).c_str());
+
+	//Csv Output for pyton plots
+	ofstream logger(filePath+"/"+fileName);
+
+	// Put Header first
+	for (unsigned i = 0; i < rewards_cumulative[0].size(); ++i)
+	{
+		logger<<"sample"<<i;
+		if(i<rewards_cumulative[0].size()-1)
+			logger<<",";
+	}
+
+	logger<<endl;
+
+	// Put datas
+	for (unsigned i = 0; i < rewards_cumulative.size(); ++i) {
+
+		for (unsigned j = 0; j < rewards_cumulative[i].size(); ++j) {
+			logger << rewards_cumulative[i][j];
+
+			if( j < rewards_cumulative[i].size()-1)
+				logger<<",";
+		}
+		logger<<endl;
+	}
+	logger.close();
+}
