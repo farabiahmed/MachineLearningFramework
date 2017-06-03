39295e38e42f13b90fbd01552ee9f307d3dac09e
farabiahmed <farabiahmed@gmail.com>
2017-06-03 10:40:32 +0300

    Unittest template added.

---
+++ unittest/template_unittest.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : template_unittest.cpp
+// Author      : FarabiAhmed
+// Version     :
+// Copyright   :
+// Description : C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void help_menu(void)
+{
+	cout << endl << endl;
+	cout << "Template Unit Test...							"<<endl;
+	cout << "---------------------------------------------------"<<endl;
+	cout << endl << endl;
+}
+
+int main()
+{
+	// Menu for user
+	help_menu();
+
+
+
+
+
+	cout<<"Done."<<endl;
+
+	return 0;
+}
