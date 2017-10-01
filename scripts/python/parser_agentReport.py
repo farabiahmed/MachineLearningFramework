1c1efe0da28fa25afacfa6f61830ff2d222e66a1
farabiahmed <farabiahmed@gmail.com>
2017-10-01 20:41:53 +0300

    Report#2 parser added.

--- scripts/python/parser_agentReport.py
+++ scripts/python/parser_agentReport.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 import sys #for commandline args
 from datetime import datetime
+import os.path
 
 # Usage:
 # python3.4 scripts/python/parser_agentReport.py
@@ -30,24 +31,40 @@ subdirectories.sort(key=lambda date: datetime.strptime(date, "%Y%m%d_%H%M%S"))
 # Get the newest directory
 freshfolder = subdirectories[-1]
 
+
+###################################
+# Report #1
+###################################
+
 # Get related file and read via pandas
 df = pd.read_csv('log/'+freshfolder+'/agentReport.csv')
 arr = df.values
 
 # Inform The User
+print("REPORT#1");
 print("Related File:", freshfolder,"/agentReport.csv")
 
 # Loop through each data and scatter
 records = arr.shape[0]
 print('Total Records',records)
+
+if records>100 :
+    skipline = (records // 100)
+else :
+    skipline = 1
+
 print('Skipline:',skipline)
 indexlist = np.arange(0,records,skipline)
+
+f1 = plt.figure()
+ax1 = f1.add_subplot(111)
 
 for xe in indexlist:
     for ye in arr[xe,1:]:
         plt.scatter(arr[xe,0],ye,color='purple', alpha=0.1)
 
 # Plot using matplotlib
+
 
 # Draw mean values
 plt.plot(arr[::skipline,0],np.mean(arr[::skipline,1:], axis=1))
@@ -56,4 +73,50 @@ plt.title('Performance Of The Agent')
 plt.xlabel('Number of Bellman Update')
 plt.ylabel('Cumulative Reward')
 plt.savefig('log/'+freshfolder+"/agentReport.svg", format="svg")
+
+###################################
+# Report #2
+###################################
+
+reportfilename = 'log/'+freshfolder+'/agentReport2.csv'
+
+if os.path.isfile(reportfilename):
+
+	# Get related file and read via pandas
+	df = pd.read_csv(reportfilename)
+	arr = df.values
+
+	# Inform The User
+	print("REPORT#2");
+	print("Related File:", freshfolder,"/agentReport2.csv")
+
+	# Loop through each data and scatter
+	records = arr.shape[0]
+	if records>100 :
+		skipline = (records // 100)
+	else :
+		skipline = 1
+	print('Total Records',records)
+
+	f2= plt.figure()
+	ax2 = f2.add_subplot(111)
+
+	# Draw Moves vs Game
+	plt.plot(arr[::skipline,0],arr[::skipline,1],color='LimeGreen')
+	plt.title('Performance Of The Agent')
+	plt.xlabel('Game #')
+	plt.ylabel('Player Moves')
+	plt.savefig('log/'+freshfolder+"/agentReport_gamevsmove.svg", format="svg")
+
+
+
+	f3= plt.figure()
+
+	plt.plot(arr[::skipline,0],arr[::skipline,2],color='OrangeRed')
+	plt.title('Exploration-Exploitation Parameter')
+	plt.xlabel('Game #')
+	plt.ylabel('E-Greedy Epsilon')
+	plt.savefig('log/'+freshfolder+"/agentReport_epsilon.svg", format="svg")
+
+
 plt.show()
