6866e26e42b3a5a90eb16d410e2414963989d03c
farabiahmed <farabiahmed@gmail.com>
2018-04-08 01:08:45 +0300

    Argument headers (-,--) are removed.

--- src/Miscellaneous/CommandLineParser.cpp
+++ src/Miscellaneous/CommandLineParser.cpp
@@ -11,7 +11,10 @@ CommandLineParser::CommandLineParser(int argc, char* argv[]) {
 
 	for (int i = 1; i < argc; ++i) {
 		//cout<< "Argument #"<<i<<" "<<argv[i]<<endl;
-		dictionary.emplace(string(argv[i]),string(argv[i+1]));
+		string command;
+		if(string(argv[i]).rfind('-',0) == 0) command = string(argv[i]).substr(1);
+		if(string(argv[i]).rfind(string("--").c_str(),0) == 0) command = string(argv[i]).substr(2);
+		dictionary.emplace(command,string(argv[i+1]));
 		i++;
 	}
 
