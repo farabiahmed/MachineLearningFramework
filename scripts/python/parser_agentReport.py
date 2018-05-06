71783ab8cb839dbd2033d0520aa1cb50e2789486
farabiahmed <farabiahmed@gmail.com>
2018-05-06 08:50:08 +0300

    Parsing different models are implemented.

--- scripts/python/parser_agentReport.py
+++ scripts/python/parser_agentReport.py
@@ -5,6 +5,8 @@ import os
 import sys #for commandline args
 from datetime import datetime
 import os.path
+from os.path import basename
+from os.path import splitext
 
 # Usage:
 # python3.4 scripts/python/parser_agentReport.py
@@ -31,56 +33,65 @@ subdirectories.sort(key=lambda date: datetime.strptime(date, "%Y%m%d_%H%M%S"))
 # Get the newest directory
 freshfolder = subdirectories[-1]
 
+# 'agentReport_15.csv', 'agentReport_1.csv', 'agentReport_7.csv',....
+modelFiles = [filename for filename in os.listdir(path + '/' + freshfolder) if filename.startswith("agentReport_")]
+modelFiles.sort(key=lambda str: int(splitext(basename(str))[0].split('_')[1]))
+print(modelFiles)
 
-###################################
-# Report #1
-###################################
+index = 0
+for filename in modelFiles:
+    ###################################
+    # Report #i
+    ###################################
+    
+    index = index + 1
+    
+    # Get related file and read via pandas
+    df = pd.read_csv('log/'+freshfolder+'/'+filename)
+    arr = df.values
+    
+    # Inform The User
+    print("REPORT #", index);
+    print("Related File:", freshfolder,"/"+filename)
+    
+    # Loop through each data and scatter
+    records = arr.shape[0]
+    print('Total Records',records)
+    
+    if records>100 :
+        skipline = (records // 100)
+    else :
+        skipline = 1
+    
+    print('Skipline:',skipline)
+    indexlist = np.arange(0,records,skipline)
+    
+    f1 = plt.figure()
+    ax1 = f1.add_subplot(111)
+    
+    for xe in indexlist:
+        for ye in arr[xe,1:]:
+            plt.scatter(arr[xe,0],ye,color='purple', alpha=0.1)
+    
+    # Plot using matplotlib
+    
+    
+    # Draw mean values
+    plt.plot(arr[::skipline,0],np.mean(arr[::skipline,1:], axis=1))
+    
+    # filter_coef = 0.95
+    # #filtered = np.zeros([indexlist.shape[0],1])
+    # filtered = arr[::skipline,1]
+    # for i in indexlist:
+    # 	for j in np.arange(1,arr.shape[1]):
+    # 		filtered[i] = filtered[i] * filter_coef + (1-filter_coef) * arr[i,j]
+    # plt.plot(arr[::skipline,0],filtered)
+    
+    plt.title('Performance Of The Agent')
+    plt.xlabel('Number of Bellman Update')
+    plt.ylabel('Cumulative Reward')
+    plt.savefig('log/'+freshfolder+"/"+splitext(basename(filename))[0]+".svg", format="svg")
 
-# Get related file and read via pandas
-df = pd.read_csv('log/'+freshfolder+'/agentReport.csv')
-arr = df.values
-
-# Inform The User
-print("REPORT#1");
-print("Related File:", freshfolder,"/agentReport.csv")
-
-# Loop through each data and scatter
-records = arr.shape[0]
-print('Total Records',records)
-
-if records>100 :
-    skipline = (records // 100)
-else :
-    skipline = 1
-
-print('Skipline:',skipline)
-indexlist = np.arange(0,records,skipline)
-
-f1 = plt.figure()
-ax1 = f1.add_subplot(111)
-
-for xe in indexlist:
-    for ye in arr[xe,1:]:
-        plt.scatter(arr[xe,0],ye,color='purple', alpha=0.1)
-
-# Plot using matplotlib
-
-
-# Draw mean values
-plt.plot(arr[::skipline,0],np.mean(arr[::skipline,1:], axis=1))
-
-# filter_coef = 0.95
-# #filtered = np.zeros([indexlist.shape[0],1])
-# filtered = arr[::skipline,1]
-# for i in indexlist:
-# 	for j in np.arange(1,arr.shape[1]):
-# 		filtered[i] = filtered[i] * filter_coef + (1-filter_coef) * arr[i,j]
-# plt.plot(arr[::skipline,0],filtered)
-
-plt.title('Performance Of The Agent')
-plt.xlabel('Number of Bellman Update')
-plt.ylabel('Cumulative Reward')
-plt.savefig('log/'+freshfolder+"/agentReport.svg", format="svg")
 
 ###################################
 # Report #2
@@ -95,7 +106,7 @@ if os.path.isfile(reportfilename):
 	arr = df.values
 
 	# Inform The User
-	print("REPORT#2");
+	print("REPORT Type#2");
 	print("Related File:", freshfolder,"/agentReport2.csv")
 
 	# Loop through each data and scatter
@@ -126,5 +137,3 @@ if os.path.isfile(reportfilename):
 	plt.ylabel('E-Greedy Epsilon')
 	plt.savefig('log/'+freshfolder+"/agentReport_epsilon.svg", format="svg")
 
-
-plt.show()
